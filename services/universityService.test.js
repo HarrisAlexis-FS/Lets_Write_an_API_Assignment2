1455518e25327f645efa6bc08b1a28c282e97630
HarrisAlexis-FS <114839962+HarrisAlexis-FS@users.noreply.github.com>
2023-08-10 22:23:05 -0400

    API Assignment Test and Mocks passed

--- services/universityService.test.js
+++ services/universityService.test.js
@@ -6,12 +6,14 @@ jest.mock("./universityService");
 describe("University Service Test", () => {
     test("Should return universities and what country they are in", async () => {
         const result = await universityService();
-        expect(result.data).toHaveLength(9936)
+        expect(result.data).toHaveLength(9936);
+        expect(result.data[0].country).toEqual('Ukraine');
     });
 
     test("Should return universities in specified country", async () => {
         const result = await universityService();
         expect(result.data[2].country).toEqual('France');
+        expect(result.data[2].alpha_two_code).toEqual('FR');
     });
 });
 
